e5396d937641de8220425766a2b5e12709e6b960
felela <111197085+felela@users.noreply.github.com>
2022-09-12 22:26:47 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,55 +1,28 @@
 #include <stdio.h>
 
-
-
 /**
+ * main - a simple program that outputs 0-9 separated by commas
  *
- *  * main - a simple program that outputs 0-9 separated by commas
- *
- *   *
- *
- *    * Return: 0 on success
- *
- *     */
-
+ * Return: 0 on success
+ */
 int main(void)
-
 {
+	int i;
+	int j;
 
-		int i;
-
-			int j;
-
-
-
-				for (i = 48; i < 57; i++)
-
-						{
-
-									for (j = i + 1; j < 58; j++)
-
-												{
-
-																putchar(i);
-
-																			putchar(j);
-
-																						if (i != 56 || j != 57)
-
-																										{
-
-																															putchar(',');
-
-																																			putchar(' ');
-
-																																						}
-
-																								}
-
-										}
-
-					putchar('\n');
-
-						return (0);
-
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
