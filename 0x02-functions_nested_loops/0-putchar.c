cc40bdfae18f34b4c5b82c0ae5e14c61bb202272
felela <111197085+felela@users.noreply.github.com>
2022-09-15 20:32:40 +0300

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * main - Entry point
+ * Description: 'prints _putchar'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+printf("_putchar\n");
+return (0);
+}
