d4a533c5f2b0c3708380e2dd1a23456afdba9d28
felela <111197085+felela@users.noreply.github.com>
2022-10-20 14:05:58 -0700

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,23 +1,30 @@
+#ifndef _LISTS_
+#define _LISTS_
+
 #include <stdio.h>
-
-
+#include <stdlib.h>
+#include <string.h>
 
 /**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- *  * bmain - function executed before main
- *
- *   * Return: no return.
- *
- *    */
-
-
-
-void __attribute__ ((constructor)) bmain()
-
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct list_s
 {
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-		printf("You're beat! and yet, you must allow");
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-			printf(",\nI bore my house upon my back!\n");
-
-}
+#endif
